64ebfcd298a387ac922f79a307c4f83be5d53a8f
Yusuke Sato <yusuke1.sato9.git@gmail.com>
2020-12-15 10:16:30 +0900

    temp

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,34 @@
-console.log('hello world' as string);
+import * as fs from "fs";
+import "./@types/fuse-native";
+import "fuse-native";
+
+const ops = {
+  readdir: (path, cb) => {
+    if (path === "/") return cb(null, ["test"]);
+    return cb(Fuse.ENOENT);
+  },
+  getattr: (path, cb) => {
+    if (path === "/") return cb(null, stat({ mode: "dir", size: 4096 }));
+    if (path === "/test") return cb(null, stat({ mode: "file", size: 11 }));
+    return cb(Fuse.ENOENT);
+  },
+  open: (path, flags, cb) => {
+    return cb(0, 42);
+  },
+  release: (path, fd, cb) => {
+    return cb(0);
+  },
+  read: (path, fd, buf, len, pos, cb) => {
+    var str = "hello world".slice(pos, pos + len);
+    if (!str) return cb(0);
+    buf.write(str);
+    return cb(str.length);
+  },
+};
+
+const fuse = new Fuse(mnt, ops, { debug: true });
+fuse.mount((err) => {
+  fs.readFile(path.join(mnt, "test"), function (err, buf) {
+    // buf should be 'hello world'
+  });
+});
